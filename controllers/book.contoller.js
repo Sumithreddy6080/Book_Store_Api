ff52ce9b822bad97fb6229043ca6f783c53a1716
sumith reddy <sumithreddy.6080@gmail.com>
2025-03-31 21:58:59 +0530

    completed adding logic

--- controllers/book.contoller.js
+++ controllers/book.contoller.js
@@ -1,13 +1,178 @@
-// const Book = require("../models/Book.model");
+const Book = require("../models/Book.model");
 
-// // Get all books
-// // const getAllBook = async (req,res)=>{
-// //     try{
-// //         const allBooks =  await Book.find({});
-// //         if(allBooks.length >0){
-// //          res.
-// //         }
-// //     }catch(err){
 
-// //     }
-// // }
+const getAllBook = async (req,res)=>{
+    try{
+        const allBooks =  await Book.find({});
+        if(allBooks.length>0){
+         res.status(200).json({
+            status: true,
+            message: "Books fetched successfully",
+            data: allBooks
+        })
+        }else{
+            res.status(200).json({
+                status: false,
+                message: "No books found",
+                data: []
+            })
+        }
+    }catch(err){
+        res.status(500).json({
+            status: false,
+            message: "Internal server error",
+            error: err.message
+        })
+    }
+}
+
+const getById = async (req,res)=>{
+    try{
+        const id = req.params.id;
+        if(!id){
+            return res.status(400).json({
+                status: false,
+                message: "Id is required"
+            })
+        }
+        const BookByID = await Book.findById(id);
+        if(!BookByID){
+            return res.status(404).json({
+                status: false,
+                message: "Book not found"
+            })
+        }
+        res.status(200).json({
+            status: true,
+            message: "Book fetched successfully",
+            data: BookByID
+        })
+    
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({
+            status: false,
+            message: "Internal server error",
+            error: err.message
+        })
+    }
+}
+
+const createBook = async (req,res)=>{
+    try{
+        
+        const {title,  author, publishedDate,  pages} = req.body;
+        if(!title || !author || !publishedDate || !pages){
+            return res.status(400).json({
+                status: false,
+                message: "All fields are required"
+            })
+        }
+
+        const newBook = await Book.create({
+            title,
+            author,
+            publishedDate,
+            pages
+        })
+        res.status(201).json({
+            status: true,
+            message: "Book created successfully",
+            data: newBook
+        })
+
+    }catch(err){
+        console.log(err.message);
+        res.status(400).json({
+            status: false,
+            message: "Bad request",
+            error: err.message
+        })
+    }
+}
+
+const updateBook = async (req,res)=>{
+    try{
+        const id = req.params.id;
+        if(!id){
+            return res.status(400).json({
+                status: false,
+                message: "Id is required"
+            })
+        }
+        const {title,  author, publishedDate,  pages} = req.body;
+        if(!title || !author || !publishedDate || !pages){
+            return res.status(400).json({
+                status: false,
+                message: "All fields are required"
+            })
+        }
+        const updatedBook = await Book.findByIdAndUpdate(id,{
+            title,
+            author,
+            publishedDate,
+            pages
+        },{new:true})
+        if(!updatedBook){
+            return res.status(404).json({
+                status: false,
+                message: "Book not found"
+            })
+        }
+        res.status(200).json({
+            status: true,
+            message: "Book updated successfully",
+            data: updatedBook
+        })
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({
+            status: false,
+            message: "Internal server error",
+            error: err.message
+        })
+    }
+}
+
+
+const deleteBook = async (req,res)=>{
+    try{
+        const id = req.params.id;
+        if(!id){
+            return res.status(400).json({
+                status: false,
+                message: "Id is required"
+            })
+        }
+        const deletedBook = await Book.findByIdAndDelete(id);
+        if(!deletedBook){
+            return res.status(404).json({
+                status: false,
+                message: "Book not found"
+            })
+        }
+        res.status(200).json({
+            status: true,
+            message: "Book deleted successfully",
+            data: deletedBook
+        })
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({
+            status: false,
+            message: "Internal server error",
+            error: err.message
+        })
+    }
+}
+
+module.exports = {
+    getAllBook,
+    getById,
+    createBook,
+    updateBook,
+    deleteBook,
+};
--- routes/book_routes.js
+++ routes/book_routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { getAllBook, getById, createBook, updateBook, deleteBook } = require('../controllers/book.contoller');
 
-router.get('/get',);
-router.get('/get/:id',);
-router.post('/add',);
-router.put('/update/:id');
-router.delete('/delete/:id');
+router.get('/get',getAllBook);
+router.get('/get/:id',getById);
+router.post('/add',createBook);
+router.put('/update/:id',updateBook);
+router.delete('/delete/:id',deleteBook);
 
 module.exports = router;
